f6c30dc226b15cdbc6dd5a3fc78707904bd538e5
Gustavo Schip <gustavoschip@proton.me>
2023-05-25 17:51:57 +0200

    Created a error logging enviorment

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();
